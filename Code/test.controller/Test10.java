9734bf7898b45f63718632a11f569094e55b6318
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-07 19:28:48 +0900

    Create Test10.java

---
+++ Code/test.controller/Test10.java
@@ -0,0 +1,16 @@
+package test.controller;
+
+public class Test10 {
+	/*
+	 * 2차원 배열에 들어있는 데이터들 중 3의 배수만 골라내서 새로운 1차원 배열에 기록하고 출력한다.
+	 * 단 중복 값은 하나만 기록되게 한다.
+	 * 
+	 * 
+	 * int [][] array = {{12, 41, 36, 56}, {82, 10, 12, 61}, {14, 16, 18, 78}, {45, 26, 72, 23}};
+	 * int[] copyAr = new int[array의 행갯수 * 열갯수];
+	 */
+
+	public static void main(String[] args) {
+
+	}
+}
