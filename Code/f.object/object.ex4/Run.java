c374e7fe3ab8a24b30623ade50e722c26f3bc395
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-09 23:03:39 +0900

    Update Run.java

--- Code/f.object/object.ex4/Run.java
+++ Code/f.object/object.ex4/Run.java
@@ -5,27 +5,27 @@ import java.util.jar.Attributes.Name;
 
 public class Run {
 	/*
-	 * choi객체 생성 최지원, 15살, 영어60, 수학70, 국어80
-	 * kim객체 생성 김도한, 18살, 영어95, 수학50, 국어70
+	 * jeon객체 생성 제민, 23살, 영어60, 수학70, 국어80
+	 * min객체 생성 민제, 32살, 영어95, 수학50, 국어70
 	 * 
 	 */
 
 	public static void main(String[] args) {
-//		Student choi = new Student("최지원", 15, 70, 60, 80);
-//		Student kim = new Student("김도한", 18, 50, 95, 70);
+//		Student choi = new Student("제민", 15, 70, 60, 80);
+//		Student min = new Student("민제", 18, 50, 95, 70);
 //		
 //		System.out.println(choi.getName() + "님의 평균 :" + choi.getEvg());
 //		System.out.println(kim.getName() + "님의 평균 :" + kim.getEvg());
 		
 //		사용자로부터 이름, 나이, 영어점수, 수학점수, 국어점수를 입력받아
 //		평균을 구하고 출력하는 프로그램 작성
-//		ex) 이름 최지원
-//		이름 최지원
-//		나이 : 14
-//		영어점수 : 80
-//		수학점수 : 100
-//		국어점수 : 75
-//		최지원님의 평균 : 85
+//		ex) 이름 제민
+//		이름 제민
+//		나이 : 23
+//		영어점수 : 60
+//		수학점수 : 70
+//		국어점수 : 80
+//		제민님의 평균 : 70.0
 		
 		Scanner sc = new Scanner(System.in);
 		
