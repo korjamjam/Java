e9046b1286466442ed6ee322bdeba416da7baa36
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-11 17:43:50 +0900

    Update StudentController.java

--- Code/z.pratice/240711/Object4/StudentController.java
+++ Code/z.pratice/240711/Object4/StudentController.java
@@ -1,35 +1,32 @@
 package test.t240711.object4;
 
-public class StudentController {
-	private Student[] sArr = new Student[5];
-	public final int CUT_LINE = 60;
-		
-	public StudentController() {
+public class StudentMenu {
+	private StudentController ssm= new StudentController();
+	
+	public StudentMenu() {
 		super();
-		sArr[0] = new Student("김길동", "자바", 100);
-		sArr[1] = new Student("박길동", "디비", 50);
-		sArr[2] = new Student("이길동", "화면", 85);
-		sArr[3] = new Student("정길동", "서버", 60);
-		sArr[4] = new Student("홍길동", "자바", 20);
-	}
-	
-	public Student[] printStudent() {
-		return sArr;
-	}
-	
-	public int sumScore() {
-		int sum = 0;
-		for(int i = 0; i< sArr.length; i++) {
-			sum += sArr[i].getScore();
-		}
-		return sum;
 		
-	}
-	
-	public double[] avgScore() {
-		double[] avgArr = new double[2];
-		avgArr[0] = this.sumScore(); //0번째 인덱스에 모든 함수의 합
-		avgArr[1] = this.sumScore() / sArr.length; //1번째 인덱스에 평균
-		return avgArr;
-	}
+		System.out.println("==========학생 정보 출력==========");
+		Student[] stArr = ssm.printStudent();
+		for(int i = 0; i < stArr.length; i++) {
+			String st = stArr[i].inform();
+			System.out.println(st);
+		}
+		
+		System.out.println("==========학생 성적 출력==========");
+		double[] avgArr = ssm.avgScore();
+		System.out.println("학생 점수 합계 :" + (int)avgArr[0]);
+		System.out.println("학생 점수 평균 :" + avgArr[1]);
+		
+		System.out.println("==========성적 결과 출력==========");
+		for(int i = 0; i< stArr.length; i++) {
+			if(ssm.CUT_LINE > stArr[i].getScore()) {
+				System.out.println(stArr[i].getName() + "학생은 재시험 대상입니다.");
+			}
+			else {
+				System.out.println(stArr[i].getName() + "학생은 통과입니다.");
+			}
+		}
+		
+	}	
 }
