51437b7519ab46f2b2943343bf23f29c34f6f198
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-25 09:24:37 +0900

    Create Participant.java

---
+++ Code/MiniProject1/Participant.java
@@ -0,0 +1,39 @@
+public class Participant {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private int[] lottoNumbers;
+    private int prizeMoney;
+
+    public Participant(String name, int age, String phoneNumber, int[] lottoNumbers) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.lottoNumbers = lottoNumbers;
+        this.prizeMoney = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int[] getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public void setPrizeMoney(int prizeMoney) {
+        this.prizeMoney += prizeMoney;
+    }
+}
