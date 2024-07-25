810200510bf171f495089a3b6eda1e9241c7f3de
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-25 17:48:41 +0900

    Update NetflixController.java

--- Code/MainProject/NetflixController.java
+++ Code/MainProject/NetflixController.java
@@ -8,29 +8,32 @@ public class NetflixController {
 	Member[] mem = new Member[5]; // 아직 등록 되지는 않았지만 최대로 등록할 수 있는 멤버 수
 	
 	
-	public void addMember() {
-		
-		
-	}
-
-	public void deleteMember() {
-
-		
-	}
-
-	public void saveVideo() {
-
-		
-	}
-
-	public void deleteVideo() {
-
+	public void addMember() {	
 		
 	}
 	
-    public void viewMemberPlaylist() {
+	public void deleteMember() {
 		
 	}
+	
+	//회원 검색을 하면 회원 정보와 그 회원의 플레이리스트 공개
+	public void searchMember() {
+		
+		
+	}
+	
+	public void saveVideo() {
+		
+	}
+	
+	public void deleteVideo() {
+		
+	}
+	
+	public void viewMemberPlaylist() {
+		
+	}
+	
 
 }
 
