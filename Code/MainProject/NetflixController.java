bd7f4bdca7e6c520eb65f96251b7663b10cb4fa2
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-25 17:47:12 +0900

    Create NetflixController.java

---
+++ Code/MainProject/NetflixController.java
@@ -0,0 +1,37 @@
+package com.kh.control;
+
+import com.kh.vo.Member;
+import com.kh.vo.Video;
+
+public class NetflixController {
+	Video[] vid = new Video[10]; // 등록된 영화
+	Member[] mem = new Member[5]; // 아직 등록 되지는 않았지만 최대로 등록할 수 있는 멤버 수
+	
+	
+	public void addMember() {
+		
+		
+	}
+
+	public void deleteMember() {
+
+		
+	}
+
+	public void saveVideo() {
+
+		
+	}
+
+	public void deleteVideo() {
+
+		
+	}
+	
+    public void viewMemberPlaylist() {
+		
+	}
+
+}
+
+
