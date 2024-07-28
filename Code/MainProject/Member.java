63791af321b4b627e38dfc913058b75ae38dcd58
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-07-28 16:25:51 +0900

    Update Member.java

--- Code/MainProject/Member.java
+++ Code/MainProject/Member.java
@@ -1,50 +1,64 @@
 package com.kh.vo;
 
+import java.util.ArrayList;
+
 public class Member {
-	private String membername;
-	private String email;
-	private int age;
-	
-	
-	public Member() {
-		super();
-	}
+    private String membername;
+    private String email;
+    private int age;
+    private ArrayList<Video> playlist;
 
-	public Member(String membername, int age) {
-		super();
-		this.membername = membername;
-		this.age = age;
-	}
+    public Member() {
+        super();
+        this.playlist = new ArrayList<>();
+    }
 
-	public String getMembername() {
-		return membername;
-	}
+    public Member(String membername, String email, int age) {
+        super();
+        this.membername = membername;
+        this.email = email;
+        this.age = age;
+        this.playlist = new ArrayList<>();
+    }
 
-	public void setMembername(String membername) {
-		this.membername = membername;
-	}
-	
+    public String getMembername() {
+        return membername;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setMembername(String membername) {
+        this.membername = membername;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	@Override
-	public String toString() {
-		return "Member [membername=" + membername + ", age=" + age + "]";
-	}
-	
-	
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<Video> getPlaylist() {
+        return playlist;
+    }
+
+    public void addVideoToPlaylist(Video video) {
+        this.playlist.add(video);
+    }
+
+    public void removeVideoFromPlaylist(Video video) {
+        this.playlist.remove(video);
+    }
+
+    @Override
+    public String toString() {
+        return "회원 이름 : " + membername + ", 이메일 : " + email + ", age=" + age + ", 플레이리스트 : " + playlist;
+    }
 }
