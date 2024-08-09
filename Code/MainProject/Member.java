3e7e6f3f60b1fa81634a874a0822f738d55d3b1f
korjamjam <128479612+korjamjam@users.noreply.github.com>
2024-08-09 16:10:07 +0900

    Update Member.java

--- Code/MainProject/Member.java
+++ Code/MainProject/Member.java
@@ -1,64 +1,111 @@
-package com.kh.vo;
+package com.kh.control;
 
 import java.util.ArrayList;
+import com.kh.vo.Member;
+import com.kh.vo.Video;
 
-public class Member {
-    private String membername;
-    private String email;
-    private int age;
-    private ArrayList<Video> playlist;
+public class MemberController {
+	private ArrayList<Member> members = new ArrayList<>();
 
-    public Member() {
-        super();
-        this.playlist = new ArrayList<>();
-    }
+	public MemberController() {
+		super();
+		//임의의 멤버
+		members.add(new Member("전제민","1@.com",10));
+		members.add(new Member("제민","2@.com",15));
+		members.add(new Member("민","3@.com",20));
+	}
 
-    public Member(String membername, String email, int age) {
-        super();
-        this.membername = membername;
-        this.email = email;
-        this.age = age;
-        this.playlist = new ArrayList<>();
-    }
+	/*
+	<회원 저장>
+	- 최대인원수 5명으로 지정
+	- 최대인원수를 넘으면 저장 불가
+	*/
+	public void addMember(String name, String email, int age) {
+		if (members.size() < 5) {
+			members.add(new Member(name, email, age));
+			System.out.println("회원이 성공적으로 추가되었습니다.");
+		} else {
+			System.out.println("회원이 가득 찼습니다.");
+			return;
 
-    public String getMembername() {
-        return membername;
-    }
+		}
+	}
 
-    public void setMembername(String membername) {
-        this.membername = membername;
-    }
+	/*
+	<모든 회원 출력>
+	- Member클래스의 toString을 오버라이딩해 출력함.(모든 회원을 보여줌)
+	*/
+	public void printMembers() {
+		for (Member m : members) {
+			System.out.println(m);
+		}
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	/*
+	<회원 삭제>
+	- members 컬렉션을 순회하여 주어진 name과 일치하는 멤버 객체를 찾음
+	- 일치하면 remove로 회원삭제후 true 반환, 없으면 false 반환
+	*/
+	public boolean deleteMember(String name) {
+		for (Member m : members) {
+			if (m.getMembername().equals(name)) {
+				members.remove(m);
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	/*
+	<특정 회원 검색>
+	- members 컬렉션을 순회하여 주어진 name과 일치하는 멤버 객체를 찾음
+	- 찾으면 그 객체와 동영상목록을 출력하고 true 반환, 일치하는 멤버가 없으면 false 반환
+	*/
+	public boolean searchMember(String name) {
+		for (Member m : members) {
+			if (m.getMembername().equals(name)) {
+				System.out.println(m);
+				System.out.println(m.getMembername() + "님의 동영상 목록");
+				for (Video v : m.getPlaylist()) {
+					System.out.println(v);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	/*
+	<NetflixController에서 사용)
+	 members 컬렉션을 순회하여 주어진 name과 일치하는 멤버 객체를 찾고, 찾으면 그 객체를 반환
+	 일치하는 멤버가 없으면 null을 반환(해당 이름의 멤버가 없다)
+	*/
+	public Member getMemberByName(String name) {
+		for (Member m : members) {
+			if (m.getMembername().equals(name)) {
+				return m;
+			}
+		}
+		return null;
+	}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public ArrayList<Video> getPlaylist() {
-        return playlist;
-    }
-
-    public void addVideoToPlaylist(Video video) {
-        this.playlist.add(video);
-    }
-
-    public void removeVideoFromPlaylist(Video video) {
-        this.playlist.remove(video);
-    }
-
-    @Override
-    public String toString() {
-        return "회원 이름 : " + membername + " / 이메일 : " + email + " / 나이 : " + age;
-    }
+	public ArrayList<Member> getMembers() {
+		return members;
+	}
+	
+	/*
+	<모든 회원의 동영상 출력>
+	- members 컬렉션을 순회하여 주어진 name과 일치하는 멤버 객체를 찾음
+	- 찾으면 그 객체와 동영상목록을 출력
+	*/
+	public void viewMemberPlaylist() {
+		System.out.println();
+		for (Member m : members) {
+			System.out.println(m);
+			System.out.println(m.getMembername() + "님의 동영상 목록");
+			for (Video v : m.getPlaylist()) {
+				System.out.println(v);
+			}
+		}
+	}
 }
